939030a868a867bbd561cd0d1e1ae21322000124
Relfos <relfos@gmail.com>
2018-06-03 01:12:22 +0100

    Classes reorganization

--- PhantasmaBridge/Core/ChainListener.cs
+++ PhantasmaBridge/Core/ChainListener.cs
@@ -11,42 +11,7 @@ using System.Threading;
 
 namespace Phantasma.Bridge.Core
 {
-    public class Mailbox
-    {
-        public readonly string name;
-        public readonly UInt160 hash;
-        public readonly string address;
-
-        public Mailbox(string name, UInt160 hash)
-        {
-            this.name = name;
-            this.hash = hash;
-            this.address = hash.ToAddress();
-        }
-
-        public static bool ValidateMailboxName(byte[] mailbox_name)
-        {
-            if (mailbox_name.Length <= 4 || mailbox_name.Length >= 20)
-                return false;
-
-            int index = 0;
-            while (index < mailbox_name.Length)
-            {
-                var c = mailbox_name[index];
-                index++;
-
-                if (c >= 97 && c <= 122) continue; // lowercase allowed
-                if (c == 95) continue; // underscore allowed
-                if (c >= 48 && c <= 57) continue; // numbers allowed
-
-                return false;
-            }
-
-            return true;
-        }
-    }
-
-    public class BridgeManager : IBlockchainProvider
+    public class ChainListener : IBlockchainProvider
     {
         private NeoAPI neo_api;
         private byte[] contract_bytecode;
@@ -62,7 +27,7 @@ namespace Phantasma.Bridge.Core
 
         public IEnumerable<Mailbox> Mailboxes => addressMap.Values;
 
-        public BridgeManager(NeoAPI api, Transaction deployTx, uint lastBlock, Logger logger)
+        public ChainListener(NeoAPI api, Transaction deployTx, uint lastBlock, Logger logger)
         {
             this.neo_api = api;
             this.lastBlock = lastBlock;
---
+++ PhantasmaBridge/Core/Mailbox.cs
@@ -0,0 +1,39 @@
+﻿using Neo.Lux.Cryptography;
+
+namespace Phantasma.Bridge.Core
+{
+    public class Mailbox
+    {
+        public readonly string name;
+        public readonly UInt160 hash;
+        public readonly string address;
+
+        public Mailbox(string name, UInt160 hash)
+        {
+            this.name = name;
+            this.hash = hash;
+            this.address = hash.ToAddress();
+        }
+
+        public static bool ValidateMailboxName(byte[] mailbox_name)
+        {
+            if (mailbox_name.Length <= 4 || mailbox_name.Length >= 20)
+                return false;
+
+            int index = 0;
+            while (index < mailbox_name.Length)
+            {
+                var c = mailbox_name[index];
+                index++;
+
+                if (c >= 97 && c <= 122) continue; // lowercase allowed
+                if (c == 95) continue; // underscore allowed
+                if (c >= 48 && c <= 57) continue; // numbers allowed
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
